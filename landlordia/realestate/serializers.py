89b7e551d0b85043d988d1bf7174b1ad17336856
Эльберд <9260401267@mail.ru>
2024-08-06 15:51:59 +0300

    Replace '__all__' with explicit field lists in serializers

--- landlordia/realestate/serializers.py
+++ landlordia/realestate/serializers.py
@@ -5,28 +5,60 @@ from rest_framework import serializers
 class OwnerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Owner
-        fields = '__all__'
+        fields = [
+            'user',
+            'phone_number',
+            'address'
+        ]
 
 
 class PropertySerializer(serializers.ModelSerializer):
     class Meta:
         model = Property
-        fields = '__all__'
+        fields = [
+            'owner',
+            'address',
+            'property_type',
+            'description',
+            'rental_type',
+            'price',
+            'price_period',
+            'minimum_rental_value',
+            'minimum_rental_unit'
+        ]
 
 
 class TenantSerializer(serializers.ModelSerializer):
     class Meta:
         model = Tenant
-        fields = '__all__'
+        fields = [
+            'first_name',
+            'last_name',
+            'email',
+            'phone_number',
+            'address'
+        ]
 
 
 class LeaseContractSerializer(serializers.ModelSerializer):
     class Meta:
         model = LeaseContract
-        fields = '__all__'
+        fields = [
+            'property',
+            'tenant',
+            'start_date',
+            'end_date',
+            'rent_amount',
+            'rent_period',
+            'deposit_amount'
+        ]
 
 
 class PaymentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Payment
-        fields = '__all__'
+        fields = [
+            'lease',
+            'amount',
+            'payment_date'
+        ]
